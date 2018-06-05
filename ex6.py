55279b15f58abd5b98b3d9ae0c9c0ffeef8dd477
AyeNandar <andhandh1381@gmail.com>
2018-06-05 09:36:37 -0700

    My sixth exercises in python

---
+++ ex6.py
@@ -0,0 +1,14 @@
+my_name='Aed A. Shaw'
+my_age=35
+my_height=64
+my_weight=150
+my_eyes='black'
+my_teeth='white'
+my_hair='Brown'
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches all.")
+print(f"He's {my_weight} piunds heavy")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usualyy {my_teeth} depending on the  coffee.")
+
