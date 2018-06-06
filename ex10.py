2bba2ac335b8994ba1d4480460fb676d38109de3
AyeNandar <andhandh1381@gmail.com>
2018-06-06 02:46:31 -0700

    My exercises

---
+++ ex10.py
@@ -0,0 +1,9 @@
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJun"
+print("Here are the days:",days)
+print("Here are the months:",months)
+print("""There's somethong going on here.
+        With the three double quotes .
+        We'll be able to type as much as we loke.
+        Even 4 lines if we want """)
+
