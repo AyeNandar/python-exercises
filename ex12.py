15f29c3bb3bb2edc8f0843bd3d93adab21565425
AyeNandar <andhandh1381@gmail.com>
2018-06-19 21:33:51 -0700

    Update ex12

--- ex12.py
+++ ex12.py
@@ -1,8 +1,8 @@
 print("How old are you?", end=' ')
-age=input( )
+age = input( )
 print("How tall are you ?", end=' ')
 height = input( )
 print("How much do you weight ?", end=' ')
 weight = input( )
-print(f"So ,you are {age} old, {height} tall and {weight} heavy." )
+print("So ,you are", age," old,", height," tall and", weight," heavy." )
 
