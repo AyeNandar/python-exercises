37efbc557033ccc8f2218d65a1e9c33b4adff838
AyeNandar <andhandh1381@gmail.com>
2018-06-19 21:29:02 -0700

    update my files

--- ex6.py
+++ ex6.py
@@ -1,14 +1,14 @@
-my_name='Aed A. Shaw'
-my_age=35
-my_height=64
-my_weight=150
-my_eyes='black'
-my_teeth='white'
-my_hair='Brown'
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches all.")
-print(f"He's {my_weight} piunds heavy")
+my_name = 'Aed A. Shaw'
+my_age = 35
+my_height = 64
+my_weight = 150
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'Brown'
+print("Let's talk about" , my_name )
+print("He's", my_height," inches all.")
+print("He's ",my_weight," piunds heavy")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usualyy {my_teeth} depending on the  coffee.")
+print("He's got", my_eyes," eyes and ",my_hair," hair.")
+print("His teeth are usualyy " , my_teeth," depending on the  coffee.")
 
---
+++ turtle.py
@@ -0,0 +1,16 @@
+import turtle
+turtle = turtle.Turtle ( )
+for i in ["green","yellow","red","brown","purple","orange","black","brown","yellow"] * 10:
+ turtle.color(i)
+ turtle.left(4)
+ turtle.forward(100)
+ turtle.right(20)
+ turtle.forward(20)
+ turtle.backward(20)
+ turtle.left(40)
+ turtle.forward(20)
+ turtle.backward(20)
+ turtle.right(20)
+ turtle.backward(100)
+    
+
