e32c3fc3dbdfc7107d949220f7cb2d532a219843
AyeNandar <andhandh1381@gmail.com>
2018-06-20 10:50:25 -0700

    Update ex42.py

--- ex42.py
+++ ex42.py
@@ -1,77 +1,61 @@
-## Animal is-a object (yes, sort of confusing) look at the extra credit
- class Animal(object):
- pass
+class Animal(object):
+    pass
 
- ## ??
- class Dog(Animal):
+class Dog(Animal):
 
- def __init__(self, name):
- ## ??
- self.name = name
+   def __init__(self, name):
 
- ## ??
- class Cat(Animal):
+       self.name = name
 
- def __init__(self, name):
- ## ??
- self.name = name
+ 
+class Cat(Animal):
 
- ## ??
- class Person(object):
+   def __init__(self, name):
 
- def __init__(self, name):
- ## ??
- self.name = name
-
- ## Person has-a pet of some kind
- self.pet = None
-
- ## ??
- class Employee(Person):
-
- def __init__(self, name, salary):
- ## ?? hmm what is this strange magic?
- super(Employee, self).__init__(name)
- ## ??
- self.salary = salary
-
- ## ??
- class Fish(object):
- pass
-
- ## ??
- class Salmon(Fish):
- pass
-
- ## ??
- class Halibut(Fish):
- pass
+      self.name = name
 
 
- ## rover is-a Dog
- rover = Dog("Rover")
+class Person(object):
 
- ## ??
- satan = Cat("Satan")
+     def __init__(self, name):
+ 
+        self.name = name
 
- ## ??
- mary = Person("Mary")
+        self.pet = None
 
- ## ??
- mary.pet = satan
+class Employee(Person):
 
- ## ??
- frank = Employee("Frank", 120000)
+   def __init__(self, name, salary):
 
- ## ??
- frank.pet = rover
+      super(Employee, self).__init__(name)
 
- ## ??
- flipper = Fish()
+      self.salary = salary
 
- ## ??
- crouse = Salmon()
+class Fish(object):
+     pass
 
- ## ??
- harry = Halibut()
+class Salmon(Fish):
+    pass
+
+class Halibut(Fish):
+    pass
+
+rover = Dog("Rover")
+
+satan = Cat("Satan")
+
+mary = Person("Mary")
+
+
+mary.pet = satan
+
+frank = Employee("Frank", 120000)
+
+frank.pet = rover
+
+flipper = Fish()
+
+crouse = Salmon()
+
+harry = Halibut()
 
