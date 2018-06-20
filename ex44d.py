4b8b266d0048182032179f4dbfd3471fdefcdff7
AyeNandar <andhandh1381@gmail.com>
2018-06-20 11:16:53 -0700

    Update my files

--- ex44d.py
+++ ex44d.py
@@ -1,32 +1,32 @@
 class Parent(object):
 
- def override(self):
- print("PARENT override()")
+    def override(self):
+        print("PARENT override()")
 
- def implicit(self):
- print("PARENT implicit()")
+    def implicit(self):
+        print("PARENT implicit()")
 
- def altered(self):
- print("PARENT altered()")
+    def altered(self):
+        print("PARENT altered()")
 
- class Child(Parent):
+class Child(Parent):
 
- def override(self):
- print("CHILD override()")
+   def override(self):
+       print("CHILD override()")
 
- def altered(self):
- print("CHILD, BEFORE PARENT altered()")
- super(Child, self).altered()
- print("CHILD, AFTER PARENT altered()")
+def altered(self):
+    print("CHILD, BEFORE PARENT altered()")
+    super(Child, self).altered()
+    print("CHILD, AFTER PARENT altered()")
 
- dad = Parent()
- son = Child()
+dad = Parent()
+son = Child()
 
- dad.implicit()
- son.implicit()
+dad.implicit()
+son.implicit()
 
- dad.override()
- son.override()
+dad.override()
+son.override()
 
- dad.altered()
- son.altered()
+dad.altered()
+son.altered()
